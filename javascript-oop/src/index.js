73fe065aa352a20a932eebe5dd878a0aa0dd7ffe
sidik181 <sidik.komarudiansah@gmail.com>
2022-06-11 02:23:36 +0700

    add modularisasi

--- javascript-oop/src/index.js
+++ javascript-oop/src/index.js
@@ -1,44 +1,4 @@
-class Table {
-	constructor(init) {
-		this.init = init;
-	}
-
-	createHeader(data) {
-		let open = "<thead class='bg-light'><tr>";
-		let close = "</tr></thead>";
-		data.forEach((d) => {
-			open += `<th>${d}</th>`;
-		});
-
-		return open + close;
-	}
-
-	createBody(data) {
-		let open = "<tbody>";
-		let close = "</tbody>";
-		data.forEach((d) => {
-			open += `
-			<tr>
-				<td>${d[0]}</td>
-				<td>${d[1]}</td>
-				<td>${d[2]}</td>
-				<td>${d[3]}</td>
-			</tr>
-			`;
-		});
-
-		return open + close;
-	}
-
-	render(element) {
-		let table =
-			"<table class='table table-hover'>" +
-			this.createHeader(this.init.columns) +
-			this.createBody(this.init.data) +
-			"</table>";
-		element.innerHTML = table;
-	}
-}
+const Table = require("../Module/Table");
 
 const table = new Table({
 	columns: ["#", "First Name", "Last Name", "Email"],
